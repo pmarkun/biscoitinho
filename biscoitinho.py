dad2e51c78633a35ffe688635c6d700ad353423f
Pedro Markun <pedro@markun.com.br>
2013-11-01 01:49:45 -0200

    adicionando licensa

--- biscoitinho.py
+++ biscoitinho.py
@@ -1,4 +1,14 @@
 # -*- coding: utf-8 -*-
+# Copyright 2013 de Monitor Legislativo
+# Este arquivo é parte do programa Extrato Legislativo.
+# O Extrato Legislativo é um software livre; você pode 
+#redistribuí-lo e/ou modificá-lo dentro dos termos da GNU General Public License 
+# como publicada pela Fundação do Software Livre (FSF); na versão 3 da Licença. 
+# Este programa é distribuído na esperança que possa ser útil, mas SEM NENHUMA GARANTIA; 
+# sem uma garantia implícita de ADEQUAÇÃO a qualquer MERCADO ou APLICAÇÃO EM PARTICULAR. 
+# Veja a licença para maiores detalhes. Você deve ter recebido uma cópia da GNU General Public License,
+# sob o título "LICENCA.txt", junto com este programa, se não, acesse http://www.gnu.org/licenses/
+
 from flask import Flask, render_template
 from unidecode import unidecode
 from datetime import datetime
